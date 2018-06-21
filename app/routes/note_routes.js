d39c18f679bd196b1f261b771f9d800e59fb78ea
João Almeida <joao.almeida@atos.net>
2018-06-21 15:53:37 -0300

    api funcional

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,8 +1,74 @@
-module.exports = function (app, db) {
+var ObjectID = require('mongodb').ObjectID;
 
-    app.post('/notes', (req, res) => {
-        // You'll create your note here.
-        console.log(req.body)
-        res.send('Hello')
+module.exports = function (app, db) {
+    app.post('/user', (req, res) => {
+        const user = {
+            text: req.body.nome,
+            title: req.body.idade,
+            ano: req.body.data
+        };
+
+        db.collection('user').insert(user, (err, result) => {
+            if (err) {
+                res.send({
+                    'error': 'An error has occurred'
+                });
+            } else {
+                res.send(result.ops[0]);
+            }
+        });
     });
-};
+
+    app.get('/user/:id', (req, res) => {
+        const id = req.params.id;
+        const details = {
+            '_id': new ObjectID(id)
+        };
+        db.collection('user').findOne(details, (err, item) => {
+            if (err) {
+                res.send({
+                    'error': 'An error has occurred'
+                });
+            } else {
+                res.send(item);
+            }
+        });
+    });
+
+    app.delete('/user/:id', (req, res) => {
+        const id = req.params.id;
+        const details = {
+            '_id': new ObjectID(id)
+        };
+        db.collection('user').remove(details, (err, item) => {
+            if (err) {
+                res.send({
+                    'error': 'An error has occurred'
+                });
+            } else {
+                res.send('Note ' + id + ' deleted!');
+            }
+        });
+    });
+
+    app.put('/user/:id', (req, res) => {
+        const id = req.params.id;
+        const details = {
+            '_id': new ObjectID(id)
+        };
+        const user = {
+            text: req.body.nome,
+            title: req.body.idade,
+            ano: req.body.ano
+        };
+        db.collection('user').update(details, user, (err, result) => {
+            if (err) {
+                res.send({
+                    'error': 'An error has occurred'
+                });
+            } else {
+                res.send(user);
+            }
+        });
+    });
+}
